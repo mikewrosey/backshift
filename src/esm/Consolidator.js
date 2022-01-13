cd8e8394324279269c28e5351eec8bd73139717b
Mike <mikewrosey@gmail.com>
2022-01-13 11:27:53 -0500

    test parse response

--- src/esm/Consolidator.js
+++ src/esm/Consolidator.js
@@ -63,6 +63,50 @@ const Consolidator = function() {
     return validCount;
   }
 
+
+  function parseResponse(json) {
+    var k, columns, columnNames, columnNameToIndex, constants, parts,
+        numMetrics = json.labels.length;
+
+    columns = new Array(1 + numMetrics);
+    columnNames = new Array(1 + numMetrics);
+    columnNameToIndex = {};
+
+    columns[0] = json.timestamps;
+    columnNames[0] = 'timestamp';
+    columnNameToIndex['timestamp'] = 0;
+
+    for (k = 0; k < numMetrics; k++) {
+      columns[1 + k] = json.columns[k].values;
+      columnNames[1 + k] = json.labels[k];
+      columnNameToIndex[columnNames[1 + k]] = 1 + k;
+    }
+
+    if (json.constants) {
+      constants = {};
+      for (var c=0, len=json.constants.length, key, value, label; c < len; c++) {
+        key = json.constants[c].key;
+        value = json.constants[c].value;
+
+        // All of the constants are prefixed with the label of the associated source, but
+        // the graph definitions don't support this prefix, so we just cut the prefix
+        // off the constant name
+        parts = key.split('.');
+        if (parts.length > 1) {
+          key = parts[1];
+          constants[key] = (value === undefined? null: value);
+        }
+      }
+    }
+
+    return {
+      columns: columns,
+      columnNames: columnNames,
+      columnNameToIndex: columnNameToIndex,
+      constants: constants
+    };
+  }
+
   function wrap(func) {
     return function (metricName, argument) {
       return {
@@ -70,7 +114,9 @@ const Consolidator = function() {
         functionName: func.name,
         argument: argument,
 
-        consolidate: function (data) {
+        consolidate: function (measurementResponse) {
+          const data = parseResponse(measurementResponse)
+
           return func(data.columns[data.columnNameToIndex["timestamp"]],
                       data.columns[data.columnNameToIndex[metricName]],
                       argument);
